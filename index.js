cef273fd8304323d04b2a43960ac4981b7286228
vittorix <vittorix@gmail.com>
2024-05-17 23:35:40 +0200

    slider - role and company

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 let images = [
   {
     src: "./images/here_maps.webp",
-    description: "Software Engineer 3 - Chicago, Illinois 2008-2012"
+    role: "Software Engineer III",
+    company: "Nokia",
+    description: "Software Engineer III - Chicago, Illinois 2008-2012"
   },
   {
     src: "./images/vittorio_at_work.jpg",
+    role: "Lead Software Engineer",
+    company: "Epsilon Data Management",
     description: "Lead Software Engineer - Chicago, Illinois 2012-2016"
   },
   {
     src: "./images/ge_transportation.jpg",
+    role: "Sr Software Engineer",
+    company: "General Electric",
     description: "Senior Software Engineer - Melbourne, Florida 2017-2018"
   }
 ];
@@ -20,6 +26,8 @@ const imageElement = document.querySelector(".image");
 const descriptionElement = document.querySelector(".description");
 const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
+const sliderRole = document.getElementById("my-pic-text-role");
+const sliderCompany = document.getElementById("my-pic-text-company");
 
 prevButton.addEventListener("click", function () {
   if (index > 0) {
@@ -41,6 +49,8 @@ function updateImage(n) {
   descriptionElement.textContent = images[n].description;
   prevButton.style.visibility = n > 0 ? "visible" : "hidden";
   nextButton.style.visibility = n < numImages - 1 ? "visible" : "hidden";
+  sliderRole.textContent = images[n].role;
+  sliderCompany.textContent = images[n].company;
 }
 
 updateImage(initialPicIndex);
