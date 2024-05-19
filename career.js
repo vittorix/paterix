97f64f5df81b92d8145bf70ae58939322b17a444
vittorix <vittorix@gmail.com>
2024-05-19 20:56:35 +0200

    career

---
+++ career.js
@@ -0,0 +1,17 @@
+// let nav = document.querySelectorAll("nav");
+
+// if (nav) {
+//   let navLinks = document.querySelectorAll("nav a");
+//   let activeLink = document.querySelector(".active");
+//   activeLink.scrollIntoView({ behavior: "smooth", inline: "center" });
+
+//   navLinks.forEach(function (link) {
+//     link.addEventListener("click", (event) => {
+//       navLinks.forEach(function (link) {
+//         link.classList.remove("active");
+//       });
+//       link.classList.add("active");
+//       link.scrollIntoView({ behavior: "smooth", inline: "center" });
+//     });
+//   });
+// }
